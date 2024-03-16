eddf5d9c3ba7aadb49648e2aad80708df2dca202
vanilla-dd <freeidlll6@gmail.com>
2024-03-17 00:30:47 +0530

    auth setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+	import { page } from '$app/stores';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+</script>
+
+<h1>SvelteKit Auth Example</h1>
+<div>
+	{#if $page.data.session}
+		{#if $page.data.session.user?.image}
+			<img src={$page.data.session.user.image} class="avatar" alt="User Avatar" />
+		{/if}
+		<span class="signedInText">
+			<small>Signed in as</small><br />
+			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+		</span>
+		<SignOut>
+			<div slot="submitButton" class="buttonPrimary">Sign out</div>
+		</SignOut>
+	{:else}
+		<span class="notSignedInText">You are not signed in</span>
+		<SignIn>
+			<div slot="submitButton" class="buttonPrimary">Sign in</div>
+		</SignIn>
+		<SignIn provider="github" />
+	{/if}
+</div>
