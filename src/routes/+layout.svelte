7f829e22ab7a119e140c3d2c1173e16c9a2f0b74
vanilla-dd <freeidlll6@gmail.com>
2024-03-17 19:38:38 +0530

    ui: font setup + navbar skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,29 @@
 	import { Moon, Sun } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Toaster } from 'svelte-sonner';
+	import { page } from '$app/stores';
 </script>
 
 <Toaster />
 <ModeWatcher />
-<main class="px-4 py-2">
-	<nav class="flex items-center justify-between">
-		<div>Logo</div>
+<main class="font-popins px-4">
+	<nav class="flex items-center justify-between border-b py-2">
+		<div>
+			<a
+				class="border-2 px-2 py-1 text-xl font-bold tracking-widest underline underline-offset-4 transition-colors hover:border-black hover:dark:border-white"
+				href="/"
+			>
+				Expensify.
+			</a>
+		</div>
 		<div class="flex items-center justify-center gap-4">
-			<p>userProfile</p>
+			<a class="h-10 w-10" href="/settings">
+				<img
+					class="h-full w-full rounded-md"
+					src={$page.data.session?.user?.image}
+					alt="userProfile"
+				/>
+			</a>
 			<Button on:click={toggleMode} variant="outline" size="icon">
 				<Sun
 					class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
