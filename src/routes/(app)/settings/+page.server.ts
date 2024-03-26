2c7cbefbd80630561f488d3c502a87593c730358
vanilla-dd <freeidlll6@gmail.com>
2024-03-26 22:16:35 +0530

    ui: bit of settings page

--- src/routes/(app)/settings/+page.server.ts
+++ src/routes/(app)/settings/+page.server.ts
@@ -3,13 +3,26 @@ import { db } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
+	const today = new Date();
+	today.setHours(0, 0, 0, 0); // Set hours to beginning of the day
+
+	const tomorrow = new Date(today);
+	tomorrow.setDate(tomorrow.getDate() + 1);
+
 	const user = await locals.auth();
 	if (!user?.user) {
-		throw redirect(301, '/');
+		throw redirect(301, '/signin');
 	}
 	const userProfile = await db.user.findUnique({
 		where: { id: user.user.id },
-		include: { expenses: true }
+		include: { HighestSpending: true, Budget: true, personalizedTag: true }
 	});
-	return { userProfile };
+	const spendedToday = await db.expenses.findMany({
+		where: {
+			userId: user.user.id,
+			createdAt: { gte: today, lt: tomorrow }
+		}
+	});
+
+	return { userProfile: { ...userProfile, spendedToday } };
 };
--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import { cn } from '$lib/utils.js';
 	import { Pencil } from 'lucide-svelte';
 	export let data;
 </script>
@@ -31,14 +32,19 @@
 	<div
 		class="group/bento row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none"
 	>
-		<div
-			class="bg-dot-black/[0.2] dark:bg-dot-white/[0.2] flex h-full min-h-[6rem] w-full flex-1 rounded-xl border border-transparent dark:bg-black"
-		>
-			44$ fsdff
+		<div class="grid h-full text-7xl">
+			<p class="flex self-end">
+				{data.userProfile?.Budget?.amount ?? 0} <span class="self-start text-xl"> $ </span>
+			</p>
+			<p class="place-self-center self-end text-xl font-bold tracking-wider text-red-600">vs</p>
+			<p class="flex place-self-end self-start">
+				{data.userProfile.spendedToday.reduce((acc, curr) => acc + curr.amount, 0)}
+				<span class="self-start text-xl"> $ </span>
+			</p>
 		</div>
 		<div class="transition duration-200 group-hover/bento:translate-x-2">
 			<div class="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-				Spended Today
+				Monthly Budget vs Today Expenses
 			</div>
 			<div class="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300"></div>
 		</div>
@@ -47,9 +53,21 @@
 		class="group/bento row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none"
 	>
 		<div
-			class="bg-dot-black/[0.2] dark:bg-dot-white/[0.2] 0 flex h-full min-h-[6rem] w-full flex-1 rounded-xl border border-transparent dark:bg-black"
+			class="bg-dot-black/[0.2] dark:bg-dot-white/[0.2] 0 flex w-full rounded-xl border border-transparent capitalize dark:bg-black"
 		>
-			320$ on fsdfsdfsfs
+			On {data.userProfile?.HighestSpending?.tagName ?? 'nothing'}
+			{data.userProfile?.HighestSpending?.tagEmoji ?? ''}
+		</div>
+		<div
+			class={cn('flex justify-center text-center text-9xl', {
+				'text-red-500':
+					data.userProfile?.HighestSpending && data.userProfile?.HighestSpending?.amount > 500,
+				'text-green-500':
+					data.userProfile?.HighestSpending && data.userProfile?.HighestSpending?.amount < 500
+			})}
+		>
+			{data.userProfile?.HighestSpending?.amount ?? 0}
+			<span class="text-3xl text-zinc-400">$</span>
 		</div>
 		<div class="transition duration-200 group-hover/bento:translate-x-2">
 			<div class="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
